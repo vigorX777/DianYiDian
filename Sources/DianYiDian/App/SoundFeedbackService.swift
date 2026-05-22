73771d4f03d32aefc6aad68af30194d2508292e9
vigorX777 <vigorxu@outlook.com>
2026-05-22 10:30:41 +0800

    fix: make check-in sound audible

--- Sources/DianYiDian/App/SoundFeedbackService.swift
+++ Sources/DianYiDian/App/SoundFeedbackService.swift
@@ -3,14 +3,21 @@ import DianYiDianCore
 
 @MainActor
 final class SoundFeedbackService {
-    private let incrementSoundName = NSSound.Name("Pop")
-    private let goalReachedSoundName = NSSound.Name("Glass")
+    private let incrementSound: NSSound?
+    private let goalReachedSound: NSSound?
+
+    init() {
+        self.incrementSound = Self.loadSystemSound(named: "Ping")
+        self.goalReachedSound = Self.loadSystemSound(named: "Glass")
+        self.incrementSound?.volume = 1
+        self.goalReachedSound?.volume = 1
+    }
 
     func playIncrementIfNeeded(settings: AppSettings) {
         guard settings.showIncrementFeedback else {
             return
         }
-        NSSound(named: incrementSoundName)?.play()
+        play(incrementSound)
     }
 
     func playGoalReachedIfNeeded(
@@ -23,6 +30,25 @@ final class SoundFeedbackService {
         else {
             return
         }
-        NSSound(named: goalReachedSoundName)?.play()
+        play(goalReachedSound)
+    }
+
+    private func play(_ sound: NSSound?) {
+        guard let sound else {
+            return
+        }
+        if sound.isPlaying {
+            sound.stop()
+        }
+        sound.currentTime = 0
+        sound.play()
+    }
+
+    private static func loadSystemSound(named name: String) -> NSSound? {
+        let url = URL(fileURLWithPath: "/System/Library/Sounds/\(name).aiff")
+        if let sound = NSSound(contentsOf: url, byReference: true) {
+            return sound
+        }
+        return NSSound(named: NSSound.Name(name))
     }
 }
